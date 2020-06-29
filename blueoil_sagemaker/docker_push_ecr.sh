d4dabc149698130d0f9995bc021d406ed023ee3d
Hideaki Masuda <masuda@leapmind.io>
2020-06-29 11:01:27 +0900

    Copied from https://github.com/awslabs/amazon-sagemaker-examples/blob/fe19e7b55fb92b4ef2aad92444870f899b0ef73d/advanced_functionality/tensorflow_bring_your_own/tensorflow_bring_your_own.ipynb

---
+++ blueoil_sagemaker/docker_push_ecr.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Copied from https://github.com/awslabs/amazon-sagemaker-examples/blob/master/advanced_functionality/tensorflow_bring_your_own/tensorflow_bring_your_own.ipynb
+# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+# The name of our algorithm
+algorithm_name=sagemaker-tf-cifar10-example
+
+cd container
+
+chmod +x cifar10/train
+chmod +x cifar10/serve
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+region=${region:-us-west-2}
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --region ${region} --no-include-email)
+
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
+
+docker build  -t ${algorithm_name} .
+docker tag ${algorithm_name} ${fullname}
+
+docker push ${fullname}
